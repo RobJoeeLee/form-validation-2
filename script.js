c774c3e34150a336aba30a0a06484a59c4e8352e
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2024-12-26 08:10:30 -0500

    created error messages

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ const errorElement = document.getElementById("error");
 form.addEventListener("submit" , (e) => {
     let messages = [];
 
+    if(name.value === "" || name.value === null){
+        messages.push("Name is required")
+    }
+
+    if(password.value.length <= 6){
+        messages.push("password must be longer than 6 characters")
+    }
+
+    if(password.value === "password"){
+        messages.push("Password cannot be password")
+    }
     if(messages.length > 0){
         e.preventDefault()
-        errorElement.innerText = messages.join(" ,");
+        errorElement.innerText = messages.join(", ");
     }
 })
