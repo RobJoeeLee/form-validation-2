1f2de9929223ec326c24709b93b2721c2bd3659d
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2024-12-26 08:03:35 -0500

    set up the event listener for the submit button in the form

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const name = document.getElementById("name");
 const password = document.getElementById("password");
 const form = document.getElementById("form");
 const errorElement = document.getElementById("error");
+
+form.addEventListener("submit" , (e) => {
+    
+})
